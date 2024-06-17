54fa542f86780f8df361f96a975e8aa2e74f94d5
Abhidwivedi183 <abhishekdwivedi28062003@gmail.com>
2024-06-17 02:44:07 -0700

    first commit

---
+++ firstpro/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Accordian from './component/accordian'
+
+
+function App() {
+ return(
+  <div className='container'>
+<Accordian/>
+  </div>
+ )
+  
+}
+
+export default App
---
+++ firstpro/src/component/accordian/data.js
@@ -0,0 +1,11 @@
+const data = [
+  {
+    name:"msd",
+    jno:"7",
+  },
+  {
+    name:"kohli",
+    jno:"18",
+  },
+]
+export default data;
